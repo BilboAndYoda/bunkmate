00287c0668d9268152951d1addd03275d118cc28
Zach Akin <zakin13@apu.edu>
2017-02-16 04:54:06 +0000

    zakin's commit before presentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+
+  get 'student/matches'
+
+  get 'student/contact'
+
+  get 'student/dorm'
+  
+  get 'student/student_home'
+
   root :to => 'home#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
   get '/sign-in' => "devise/sessions#new", :as => :login
-  get '/roommatecontactinfo' => ""
   end
+  
 end
