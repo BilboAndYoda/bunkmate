c2b488310fd746b3f6f81c54a22ef4c77e5c64cb
BilboAndYoda <jordboudreau@gmail.com>
2017-03-29 20:05:30 +0000

    commiting before switching branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   
   get 'questionnaire/index'
 
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   get 'admin/profile'
 
   get 'admin/students'
+
 
   get 'student/matches'
 
